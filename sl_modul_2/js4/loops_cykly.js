18b9cdde23c896d9d4f99e0b065cf0e23ca0ee12
ViktorIgnatiev <viktor.ignatiev@wp.pl>
2025-04-24 22:18:43 +0200

    24.04.2025 vol 2

--- sl_modul_2/js4/loops_cykly.js
+++ sl_modul_2/js4/loops_cykly.js
@@ -12,32 +12,63 @@
     */
 
 
-console.log("Before");
-
-for (let i = 0; i < 100; i++) {
-    console.log('Loop running 1');
-    ;
-    
-}
-// i++ це те само що +1
-console.log("After");
-
-
-/**
-    * ______
-    */
-
-
 // console.log("Before");
+
+// for (let i = 0; i < 100; i++) {
+//     console.log('Loop running 1');
+//     ;
+    
+// }
+// // i++ це те само що +1
 // console.log("After");
 
 
 /**
-    * ______
+    ** __While - цикл____поьрібно в аргумент передавати умову що повертає булеве значення
+    перше виконує потім перевіряє умову
     */
 
 
-let a = 10;
+// console.log("Before");
+
+// let number = 25
+
+// while (number > -20) {
+//     console.log("executing...", number >-20, number);
+//     number -= 5; 
+// }
+
+// console.log("After");
+
+
+/**
+   * * ______doWhile цикл 
+    */
+
+// let a = 10;
 
 // console.log(a);
 // console.log(b);
+
+// *================
+
+// console.log("before while loop");
+
+// let counter = 10;
+
+// console.log("after while loop");
+
+
+
+
+
+
+// *** різниця між  while i do while 
+// while (false) {
+//     console.log("some othher code");
+// } 
+// // *******
+// do {
+//     console.log("some code");
+    
+// } while (false);
